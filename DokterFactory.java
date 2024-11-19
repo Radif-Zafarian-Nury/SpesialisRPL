24cd9b22b58145cc4f614c361a68f0b90ddfc439
RengganaKurniawan <6182201071@student.unpar.ac.id>
2024-11-19 11:00:00 +0700

    add Spring

--- DokterFactory.java
+++
@@ -1,3 +0,0 @@
-public class DokterFactory implements Staff {
-    
-}
---
+++ spesialisRPL/src/main/java/com/example/spesialisRPL/SpesialisRplApplication.java
@@ -0,0 +1,13 @@
+package com.example.spesialisRPL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpesialisRplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpesialisRplApplication.class, args);
+	}
+
+}
