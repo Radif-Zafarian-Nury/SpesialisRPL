f457b0c9a0677c24ff7cee970b9c0ac59c9e9843
carolynchrstn <carolyn.christina1@gmail.com>
2024-12-06 16:54:16 +0700

    Updated SQL queries

--- Data/views_WomLinic.sql
+++ Data/views_WomLinic.sql
@@ -7,14 +7,12 @@ CREATE VIEW lihat_jadwal_dokter AS
 (SELECT
 	nama,
 	nama_spesialisasi,
-	hari,
+	tanggal,
 	waktu_mulai,
-	waktu_selesai,
+	waktu_selesai
 FROM
-	users INNER JOIN jadwal_dokter
-	ON users.id_user = jadwal_dokter.id_dokter
-	INNER JOIN jadwal
-	ON jadwal.id_jadwal = jadwal_dokter.id_jadwal
+	users INNER JOIN jadwal
+	ON users.id_user = jadwal.id_dokter
 	INNER JOIN spesialisasi_dokter
 	ON spesialisasi_dokter.id_dokter = users.id_user
 	INNER JOIN spesialisasi
@@ -24,6 +22,7 @@ FROM
 CREATE VIEW daftar_dokter AS
 (SELECT
 	nama,
+	foto_dokter,
 	nama_spesialisasi
 FROM
 	users INNER JOIN spesialisasi_dokter
