737c85e8630080762fdfe10181c0d90ccc598c5e
6182201091 <6182201091@student.unpar.ac.id>
2024-12-11 10:38:55 +0700

    List, Login cek session, register, dll,

--- spesialisRPL/src/main/java/com/example/spesialisRPL/Admin/AdminController.java
+++ spesialisRPL/src/main/java/com/example/spesialisRPL/Admin/AdminController.java
@@ -1,5 +1,7 @@
 package com.example.spesialisRPL.Admin;
 
+import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,15 +9,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.spesialisRPL.RequiredRole;
+import com.example.spesialisRPL.User.UserData;
 import com.example.spesialisRPL.User.UserService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/admin")
