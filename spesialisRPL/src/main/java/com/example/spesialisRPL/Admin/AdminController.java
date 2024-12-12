eb6745d6d79365bcac533499524c846ee8aa3f7d
carolynchrstn <carolyn.christina1@gmail.com>
2024-12-12 22:37:50 +0700

    Merge branch

--- spesialisRPL/src/main/java/com/example/spesialisRPL/Admin/AdminController.java
+++ spesialisRPL/src/main/java/com/example/spesialisRPL/Admin/AdminController.java
@@ -10,15 +10,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.spesialisRPL.RequiredRole;
+import com.example.spesialisRPL.User.UserData;
 import com.example.spesialisRPL.User.UserService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/admin")
